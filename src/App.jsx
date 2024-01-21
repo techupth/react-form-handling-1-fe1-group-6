2c817ea9bd9793775be104cac5521d219f96c814
GaiKT <124340777+GaiKT@users.noreply.github.com>
2024-01-21 10:12:50 +0700

    feat: usestate with userinput update state

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [message , setMessage] = useState('Greeting Message')
+  const [input , setInput] = useState('')
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{message}</div>
       <div className="input-container">
         <label htmlFor="greeting-message">New Greeting Message</label>
-        <input id="greeting-message" type="text" />
+        <input 
+        id="greeting-message" 
+        type="text" 
+        onChange={(event)=>{
+          setInput(event.target.value)
+        }}
+        value={input}
+        />
       </div>
 
       <div className="buttons">
-        <button>Update text</button>
+        <button onClick={()=>{
+          setMessage(input)
+        }}>Update text</button>
       </div>
     </div>
   );
